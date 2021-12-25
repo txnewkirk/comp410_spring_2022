340c97a8e8c29ab4415d0537dc9785b0ccabfc6d
mkc <claesmk@icloud.com>
2021-12-25 17:23:15 -0500

    Added ability to read data into a list without newlines

---
+++ pii_data.py
@@ -0,0 +1,15 @@
+import re
+
+
+def read_data(filename: str):
+    data = []
+    with open(filename) as f:
+        # Read one line from the file stripping off the \n
+        for line in f:
+            data.append(line.rstrip())
+    return data
+
+
+if __name__ == '__main__':
+    data = read_data('sample_data.txt')
+    print(data)
---
+++ test_pii_data.py
@@ -0,0 +1,16 @@
+import unittest
+from pii_data import read_data
+
+
+class DataTestCases(unittest.TestCase):
+    def test_read_data(self):
+        expected_data = ['Aggie Pride Worldwide',
+                         'Aggies Do']
+
+        data = read_data('sample_data.txt')
+
+        self.assertEqual(data, expected_data)
+
+
+if __name__ == '__main__':
+    unittest.main()
