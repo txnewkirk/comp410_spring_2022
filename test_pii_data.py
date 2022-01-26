ea1026fd1d10be25df296efc151170cf6f48aaf7
znhinson <znhinson@aggies.ncat.edu>
2022-01-26 11:18:51 -0500

    Implemented my slogan

--- test_pii_data.py
+++ test_pii_data.py
@@ -5,7 +5,7 @@ from pii_data import read_data
 class DataTestCases(unittest.TestCase):
     def test_read_data(self):
         expected_data = ['Aggie Pride Worldwide',
-                         'Aggies Do']
+                         'Aggies Do','Way To Go Aggies']
 
         data = read_data('sample_data.txt')
 
